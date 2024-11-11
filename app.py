e92c78331a9c605c7b53b5df738a373f43f498ae
RJ Conanan <rauljarodconanan@gmail.com>
2024-11-11 18:36:32 +0800

    added simple flask app to demonstrate encryption and decryption

---
+++ app.py
@@ -0,0 +1,40 @@
+import json
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from managers import EncryptionKeyManager
+from helpers import CollectionGetter
+
+
+app = Flask(__name__)
+
+try:
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+        hostname = config['hostname']
+        port = config['port']
+        database = config['database']
+        n_applications = config['n_applications']
+        credit_transaction_size = config['credit_transaction_size']
+except Exception as e:
+    raise e
+
+client = MongoClient(f"mongodb://{hostname}:{port}/")
+db = client[database]
+em = EncryptionKeyManager('encryption_key.key')
+
+
+@app.route("/get_application/<string:item_id>/", methods=["GET"])
+def get_application(item_id: str):
+    application_getter = CollectionGetter(client=client, database=database, collection='applications',
+                                          encryption_manager=em, is_encrypted=True)
+    item = application_getter.get_item(item_id=item_id)
+    return item
+
+
+@app.route("/get_user_profile/<string:item_id>/", methods=["GET"])
+def get_user_profile(item_id: str):
+    profile_getter = CollectionGetter(client=client, database=database, collection='user_profiles',
+                                      encryption_manager=em, is_encrypted=True)
+    item = profile_getter.get_item(item_id=item_id)
+    return item
