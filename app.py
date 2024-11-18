89f026e39544d8fa62a334910b1dc4b2979d8ce2
RJ Conanan <rauljarodconanan@gmail.com>
2024-11-18 12:57:06 +0800

    add test view for application deletion and update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from managers import EncryptionKeyManager
-from helpers import CollectionGetter, CollectionPoster
+from helpers import CollectionGetter, CollectionPoster, CollectionDeleter, CollectionUpdater
 
 
 app = Flask(__name__)
@@ -44,6 +44,27 @@ def create_application():
         return result
     except Exception as e:
         return jsonify({"error": "Failed to post item", "details": str(e)}), 500
+
+
+@app.route('/update/application/<string:item_id>/', methods=['PATCH'])
+def update_application(item_id:str):
+    cu = CollectionUpdater(client=client, database=database, collection='applications', encryption_manager=em,
+                           is_encrypted=True)
+    try:
+        data = request.json
+        if not data:
+            return jsonify({"error": "Invalid input"}), 400
+        result = cu.handle_item(item_id=item_id, data=data)
+        return result
+    except Exception as e:
+        return jsonify({"error": "Failed to update item", "details": str(e)}), 500
+
+
+@app.route('/delete/application/<string:item_id>/', methods=['DELETE'])
+def delete_application(item_id: str):
+    cd = CollectionDeleter(client=client, database=database, collection='applications')
+    result = cd.handle_item(item_id=item_id)
+    return result
 
 
 @app.route("/retrieve/user_profile/<string:item_id>/", methods=["GET"])
